f9facd9cdc34b23f465a64ae61d512c4c9c20c63
Lydsi-Pi <future.lydsi@gmail.com>
2021-06-16 15:19:41 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -16,26 +16,7 @@ function readyDiscord() {
     console.log('Kotori-bot ready to go master. 💖');
 }
 
-bot.on('message', gotMessage);
+const commandHandler = require("./commands");
 
-async function gotMessage(msg) {
+bot.on('message', commandHandler);
 
-        let tokens = msg.content.split(" ");
-    
-    if (tokens[0] === 'hi') {
-        msg.channel.send('おはよう Ohayo! ^-^ 💕');
-    } else if (tokens[0] == '!gif') {
-
-        let keywords = 'anime girl';
-        if (tokens.length > 1) {
-            keywords = tokens.slice(1, tokens.length).join(" ");
-        }
-      
-        let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&ContentFilter=low`;
-        let response = await fetch(url);
-        let json = await response.json();
-        const index = Math.floor(Math.random() * json.results.length);
-
-        msg.channel.send(json.results[index].url);
-    }
-}
